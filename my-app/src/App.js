36099c201b6b096c61ea191808ef0e452b989bf6
meganii <yuhei24@gmail.com>
2019-02-09 07:52:55 +0900

    Add posts

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,15 @@
 import './App.css';
 
 import React from 'react';
-import { Admin, Resource } from 'react-admin';
+import { Admin, Resource, ListGuesser } from 'react-admin';
+import { PostList } from './posts';
 import { UserList } from './users';
 import jsonServerProvider from 'ra-data-json-server';
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 
 const App = () => (
   <Admin dataProvider={dataProvider}>
+    <Resource name="posts" list={PostList} />
     <Resource name="users" list={UserList} />
   </Admin>
 );
