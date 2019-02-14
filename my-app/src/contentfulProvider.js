5e40b201f0819b042591c406978bf2c961c2cc3c
meganii <yuhei24@gmail.com>
2019-02-14 23:54:54 +0900

    Update entry

--- my-app/src/contentfulProvider.js
+++ my-app/src/contentfulProvider.js
@@ -1,4 +1,5 @@
 import { stringify } from 'query-string'
+import * as contentful from 'contentful-management'
 import {
   GET_LIST,
   GET_ONE,
@@ -13,39 +14,16 @@ const accessToken = process.env.REACT_APP_CONTENTFUL_DELIVERY_TOKEN
 const spaceId = process.env.REACT_APP_CONTENTFUL_SPACE_ID
 const apiUrl = `https://cdn.contentful.com/spaces/${spaceId}/environments/master`
 
-export default (type, resource, params) => {
-  let url = ''
+const client = contentful.createClient({
+  accessToken: process.env.REACT_APP_CONTENTFUL_CONTENT_MANAGEMENT_API_KEY
+})
+
+export default　async (type, resource, params) => {
   switch (type) {
     case GET_LIST: {
-      url = `${apiUrl}/${resource}?access_token=${accessToken}`;
-      break;
-    }
-    case GET_ONE: {
-      url = `${apiUrl}/${resource}/${params.id}?access_token=${accessToken}`;
-      break;
-    }
-  }
-
-  return fetch(url)
-    .then(res => res.json())
-    .then(response => {
-      console.log(response)
-
-      if(!response) {
-        console.error()
-      }
-      if(!response.items) {
-        return {
-          data: {
-            id: response.sys.id,
-            title: response.fields.title,
-            slug: response.fields.slug,
-            body: response.fields.body         
-          }
-        }
-      }
-
-      const data = response.items.map(item => {
+      const res = await fetch(`${apiUrl}/${resource}?access_token=${accessToken}`)
+      const response = await res.json()
+      const data = await response.items.map(item => {
         return {
           id: item.sys.id,
           title: item.fields.title,
@@ -56,5 +34,47 @@ export default (type, resource, params) => {
         data: data,
         total: response.total
       }
-  })
+    }
+    case GET_ONE: {
+      const res = await fetch(`${apiUrl}/${resource}/${params.id}?access_token=${accessToken}`)
+      const response = await res.json()
+      return {
+        data: {
+          id: response.sys.id,
+          title: response.fields.title,
+          slug: response.fields.slug,
+          body: response.fields.body         
+        }
+      }
+    }
+    case UPDATE: {
+      const space = await client.getSpace(spaceId)
+      const environment = await space.getEnvironment('master')
+      let entry = await environment.getEntry(params.id)
+      entry.fields.title = {'ja-JP': params.data.title}
+      const updatedEntry = await entry.update()
+      const publishedEntry = await updatedEntry.publish()
+      return {
+        id: publishedEntry.id,
+        data: {
+          id: publishedEntry.fields.id,
+          title: publishedEntry.fields.title,
+          slug: publishedEntry.fields.slug,
+          body: publishedEntry.fields.body
+        },
+        previousData: {
+          id: entry.id,
+          data: {
+            id: entry.fields.id,
+            title: entry.fields.title,
+            slug: entry.fields.slug,
+            body: entry.fields.body
+          }
+        }
+      }
+    }
+    default: {
+      return {data: []}
+    }
+  }
 }
